ba9058009a7d916577ea1f36727edb346dbc091c
lailalelouch <39729644+lailalelouch@users.noreply.github.com>
2019-06-17 18:44:54 +0300

    Update urls.py

--- the_reading_potato/urls.py
+++ the_reading_potato/urls.py
@@ -20,9 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', include('main.urls')),
-    path('', include('authentication.urls')),
 ]
 
 
