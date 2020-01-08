3d49a4a9e1a0f92777211011b0ba0ed9cfd7c3de
Fay <fayalzouman@gmail.com>
2020-01-08 18:07:32 +0300

    finished login functionality

--- authentication/forms.py
+++ authentication/forms.py
@@ -10,3 +10,7 @@ class UserRegister(forms.ModelForm):
         widgets={
             'password': forms.PasswordInput(),
         }
+        
+class UserLogin(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput())
